da648607100cb3223ab50c5a395443dd6c0e0186
Vincent Aranega <vincent.aranega@gmail.com>
2017-11-25 16:32:14 +0100

    Bump version: 0.7.6 → 0.7.7-dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ packages = ['pyecore',
 
 setup(
     name='pyecore-py2',
-    version='0.7.6',
+    version='0.7.7-dev',
     description=('A Python(ic) Implementation of the Eclipse Modeling '
                  'Framework (EMF/Ecore), Python 2.7 backport'),
     long_description=open('README.rst').read(),
