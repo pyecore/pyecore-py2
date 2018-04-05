b242b5a8011bfb69bab9ac7b4f84624c04f1e353
Vincent Aranega <vincent.aranega@gmail.com>
2018-04-05 11:31:41 +0200

    Add fix, tests and multiple root support for json resources

--- pyecore/resources/xmi.py
+++ pyecore/resources/xmi.py
@@ -222,6 +222,7 @@ class XMIResource(Resource):
                 eatts.append((feature, value))
             else:
                 erefs.append((feature, value))
+        self._resolve_mem[eobject.eURIFragment()] = eobject
         return (feature_container, eobject, eatts, erefs)
 
     def _decode_attribute(self, owner, key, value):
@@ -288,6 +289,7 @@ class XMIResource(Resource):
     def _clean_registers(self):
         del self._later[:]
         self._meta_cache.clear()
+        self._resolve_mem.clear()
 
     def register_nsmap(self, prefix, uri):
         if uri in self.reverse_nsmap:
